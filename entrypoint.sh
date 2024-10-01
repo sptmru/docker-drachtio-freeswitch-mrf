b76a2fd39fa8675c240d84f3e3a7faf3bb3c7761
Soslan Aldatov <soslanaldatov@gmail.com>
2024-10-01 14:27:29 +0400

    update ulimit configuration in image entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -130,16 +130,16 @@ done
     exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 fi
 
-ulimit -c unlimited # The maximum size of core files created.
-ulimit -d unlimited # The maximum size of a process's data segment.
-ulimit -f unlimited # The maximum size of files created by the shell (default option)
-ulimit -i unlimited # The maximum number of pending signals
-ulimit -n 999999    # The maximum number of open file descriptors.
-ulimit -q unlimited # The maximum POSIX message queue size
-ulimit -u unlimited # The maximum number of processes available to a single user.
-ulimit -v unlimited # The maximum amount of virtual memory available to the process.
-ulimit -x unlimited # ???
-ulimit -s 240         # The maximum stack size
-ulimit -l unlimited # The maximum size that may be locked into memory.
+ulimit -c unlimited
+ulimit -d unlimited
+ulimit -f unlimited
+ulimit -i unlimited
+ulimit -n 999999
+ulimit -q unlimited
+ulimit -u unlimited
+ulimit -v unlimited
+ulimit -x unlimited
+ulimit -s 240
+ulimit -l unlimited
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
